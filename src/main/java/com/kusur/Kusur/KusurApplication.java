d9e85ddc34e190e9aea8dee5ba10fc67e95ed7de
AleksandarAngelevski <aleksandar.angelevski13@gmail.com>
2025-06-15 00:18:35 +0200

    Changes

--- src/main/java/com/kusur/Kusur/KusurApplication.java
+++ src/main/java/com/kusur/Kusur/KusurApplication.java
@@ -11,6 +11,7 @@ public class KusurApplication {
 	public static void main(String[] args) {
 		Dotenv dotenv = Dotenv.configure().directory("/etc/secrets").filename(".env").load();
 		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
 		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		System.setProperty("MAIL_USERNAME", dotenv.get("MAIL_USERNAME"));
 		System.setProperty("MAIL_PASSWORD", dotenv.get("MAIL_PASSWORD"));
