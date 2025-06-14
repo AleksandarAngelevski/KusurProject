108ba265133b6a4e31fec9b8ad4ea3c6bbadc272
AleksandarAngelevski <aleksandar.angelevski13@gmail.com>
2025-06-14 23:28:34 +0200

    Changes

--- src/main/java/com/kusur/Kusur/KusurApplication.java
+++ src/main/java/com/kusur/Kusur/KusurApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 public class KusurApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().directory("/etc/secrets/").load();
+		Dotenv dotenv = Dotenv.configure().directory("/etc/secrets").filename(".env").load();
 		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
 		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		System.setProperty("MAIL_USERNAME", dotenv.get("MAIL_USERNAME"));
